fce35dc1805515b49e594e87d9382cac2a22cbea
Jonas <mail@jonasmetzger.de>
2024-06-24 17:35:33 +0200

    english translation.

--- src/main/java/de/haw_hamburg/Transaction.java
+++ src/main/java/de/haw_hamburg/Transaction.java
@@ -3,15 +3,12 @@ package de.haw_hamburg;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-/**
- * Repräsentiert eine Transaktion über ein gegebenen Betrag mittels Sender und Empfänger
- */
 @Getter
 @RequiredArgsConstructor
 public class Transaction {
 
-    private final double betrag;
+    private final double amount;
     private final Account sender;
-    private final Account empfaenger;
+    private final Account receiver;
 
 }
